1b12ef3394208d09226e2babc76a3520f594b2c4
Jan <janvilaaregut@gmail.com>
2022-09-09 19:44:17 +0200

    Quizzical

--- src/components/Start.jsx
+++ src/components/Start.jsx
@@ -4,7 +4,7 @@ export default function Start(props) {
   return (
     <div className="StartComponent">
       <h1 className="title">Quizzical</h1>
-      <p className="start__p">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Hic, quia cumque amet similique deleniti maiores dolor rem eligendi! Quasi, nobis.</p>
+      <p className="start__p">Quizzical is a great app to conquer your cluelessness and  test your knowledge, or as a fun challenge with your friends (or alone). You will learn about history, science, literature and much more by answering to questions. I promise you won't leave Quizzical without learning something new!</p>
       <button className="start__btn" onClick={() => {props.startGame[1](true)}}>Start Quiz</button>
     </div>
   )
